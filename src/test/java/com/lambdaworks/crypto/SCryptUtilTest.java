74259a592c2a37bf9eb251e981c5dee96b3ea465
Will <will@glozer.net>
2011-02-04 21:07:58 +0900

    add modified modular crypt format implementation

---
+++ src/test/java/com/lambdaworks/crypto/SCryptUtilTest.java
@@ -0,0 +1,39 @@
+// Copyright (C) 2011 - Will Glozer.  All rights reserved.
+
+package com.lambdaworks.crypto;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SCryptUtilTest {
+    @Test
+    public void scrypt() {
+        int N = 16384;
+        int r = 8;
+        int p = 1;
+
+        String hashed = SCryptUtil.scrypt("secret", N, r, p);
+        String[] parts = hashed.split("\\$");
+
+        assertEquals(5, parts.length);
+        assertEquals("s0", parts[1]);
+        assertEquals(16, Base64.decodeFast(parts[3]).length);
+        assertEquals(32, Base64.decodeFast(parts[4]).length);
+
+        int params = Integer.valueOf(parts[2], 16);
+
+        assertEquals(N, (int) Math.pow(2, params >> 16 & 0xff));
+        assertEquals(r, params >>  8 & 0x0f);
+        assertEquals(p, params >>  0 & 0x0f);
+    }
+
+    @Test
+    public void check() {
+        String passwd = "secret";
+
+        String hashed = SCryptUtil.scrypt(passwd, 16384, 8, 1);
+
+        assertTrue(SCryptUtil.check(passwd, hashed));
+        assertFalse(SCryptUtil.check("s3cr3t", hashed));
+    }
+}
