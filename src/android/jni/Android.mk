6dbc37f4938ab83ca1b15badcc160f0339856602
Haochen Xie <haochen@xie.name>
2015-07-16 20:58:59 +0900

    Add prebuilt android native libraries for Android 2.3
    Signed-off-by: Haochen Xie <haochen@xie.name>

--- src/android/jni/Android.mk
+++ src/android/jni/Android.mk
@@ -1,12 +1,12 @@
 LOCAL_PATH      := $(call my-dir)
 
 SSE2 :=
-TARGET_PLATFORM := android-19
+TARGET_PLATFORM := android-9
 
 include $(CLEAR_VARS)
 LOCAL_MODULE     := scrypt
 
-LOCAL_SRC_FILES  := $(wildcard c/*.c)
+LOCAL_SRC_FILES  := $(wildcard $(LOCAL_PATH)/c/*.c)
 LOCAL_SRC_FILES  := $(filter-out $(if $(SSE2),%-nosse.c,%-sse.c),$(LOCAL_SRC_FILES))
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 
